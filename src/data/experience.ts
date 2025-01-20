6a08d05065dccd9718273738676b32cf20d46bfb
Jérémy Beaussart <jeremy.beaussart@gmail.com>
2025-01-20 22:48:22 +0100

    feat: update content

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -25,7 +25,6 @@ export const EXPERIENCE: ExperienceProp[] = [
     description:
       "Création from scratch d'un scraper de donnée en Ruby. Développement d'une application de gestion et de mise en place de sauvegardes à distance (Avamar), de supervision de divers postes (Zabbix) et de prise en main à distance (ISL). L'architecture est composée d'une API Ruby on Rails et de trois microservices en Ruby le frontend est en VueJS.",
     technologies: [
-      "Ruby",
       "Ruby on Rails",
       "Rspec",
       "VuejS",
@@ -33,8 +32,6 @@ export const EXPERIENCE: ExperienceProp[] = [
       "Zabbix",
       "Avamar",
       "Sequel",
-      "GrPC",
-      "Warden",
       "Docker",
     ],
     logo: "/logos/citronmauve.png",
